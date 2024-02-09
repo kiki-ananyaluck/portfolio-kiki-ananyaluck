a9a72c0994757bfd72b596b8eb623c9004814dee
kiki <kiki.ananyaluck@gmail.com>
2024-02-09 12:52:10 +0700

    edit padding competence

--- src/component/Competence.jsx
+++ src/component/Competence.jsx
@@ -1,12 +1,12 @@
 function Competence() {
     return (
-        <div className="h-auto bg-secondary text-accent px-10 py-10 my-14 md:my-20 md:px-[50px] md:py-[50px] min-[1680px]:px-[100px] min-[1680px]:py-[100px]">
+        <div className="h-auto bg-secondary text-accent px-10 py-10 md:my-20 md:px-[50px] md:py-[50px] min-[1680px]:py-[100px]">
             <div className="xl:flex">
                 <div className="mx-auto md:w-[600px] md:h-[450px] min-[1680px]:w-[700px] min-[1680px]:h-[500px]">
                     <img src="https://img5.pic.in.th/file/secure-sv1/cat.gif" alt="cat.gif" border="0" width="auto" height="auto" />
                 </div>
                 <div className="md:flex-1 md:px-10 min-[1680px]:py-[50px]">
-                    <h3 className="text-xl font-for-head uppercase my-10 font-black md:text-2xl md:my-20">competence</h3>
+                    <h3 className="text-xl font-for-head uppercase my-10 font-black md:text-2xl md:pt-[20px]">competence</h3>
                     <div className="md:flex">
                         <div className="mb-16 md:flex-1 ">
                             <h4 className="uppercase font-black mb-8">personal skills</h4>
