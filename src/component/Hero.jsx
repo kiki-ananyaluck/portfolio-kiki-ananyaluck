20938133fb68f21778f94fe9c6a7b8a35934616a
kiki <kiki.ananyaluck@gmail.com>
2024-02-09 14:20:55 +0700

    edit hero

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
                         <p className="uppercase text-lg md:text-3xl">ananyaluck sreesang</p>
                         <p className="capitalize text-md text-secondary md:text-[20px]">junior software developer</p>
                         <h1 className="text-6xl font-bold font-for-head py-10 md:text-[100px] md:p-[85px]">Portfolio</h1>
-                        <a href="https://drive.google.com/file/d/1Oh7rv6tqOW3Rr6cZDO7BB0cWJaliq0bY/view?usp=sharing" target="_blank" rel="noreferrer"><button className="but-style py-2 px-3 mx-[15px]  md:py-3 md:px-7">CV Download</button></a>
+                        <a href="https://drive.google.com/file/d/1GwhjfSavxhnfj4Fql3-H9Q6c6xe-kp1i/view?usp=sharing" target="_blank" rel="noreferrer"><button className="but-style py-2 px-3 mx-[15px] md:py-3 md:px-7">CV Download</button></a>
                         <a href="mailto:kiki.ananyaluck@gmail.com"><button className="but-style-one py-2 px-3 mx-[15px] w-[135px] my-[15px] md:w-[164px] md:py-3 md:px-7">Let’s Talk</button> </a>
                     </div>
                 </div>
